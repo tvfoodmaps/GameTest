671877b78cb19b4683ee896035731ddcceb8876d
Frank Apap <tvfoodmaps@gmail.com>
2015-07-15 19:09:01 -0400

    fill glass

--- app/lib/DropItem.js
+++ app/lib/DropItem.js
@@ -3,10 +3,14 @@ var platino = require('io.platino');
 module.exports = function(x,y,game,scene){
 
 	//x cooridinate needs to be adjusted for different ratios
-	var dropitem = platino.createSprite({image:'images/item_blue.png', x:game.oX(x),y:y,z:1, height:248, width:248 });
-	dropitem.alpha = .25;
-	
-	
-	return dropitem;	
+	var dropitem = platino.createSpriteSheet({image:'images/glass_sheet.png',x:game.oX(x),y:y,z:3,height:375,width:252 });
+	Ti.API.info("frame count "+dropitem.frameCount);
+	dropitem.frame = 1;
+
+	dropitem.fillGlass=function(){
+		dropitem.frame = 0;
+	};
+
+	return dropitem;
 
 };
--- app/lib/MainScene.js
+++ app/lib/MainScene.js
@@ -16,47 +16,49 @@ module.exports = function(win,game){
 	var background = platino.createSprite({image:'images/Background.png', x:game.oX(0), y:0, height:1536, width:2048 });
 	//game.setupSpriteSize(background);
 	self.add(background);
-	
+
 	var shelf = platino.createSprite({image:'images/shelf.png', x:game.oX(0), y:850,z:2 });
 	//game.setupSpriteSize(background);
 	self.add(shelf);
-	
-	
-	
+
+
+
 	Ti.API.info("background width: "+background.width);
 	var DragItem = require('DragItem');
 	var dItem1 = new DragItem(50,1200,game,self);
 	var dItem2 = new DragItem(300,1200,game,self);
-	
 
-	var dropZones = [];	
+
+	var dropZones = [];
 	var DropItem = require('DropItem');
-	var drop1 = new DropItem(0,0,game,self);
-	var drop2 = new DropItem(100,700,game,self);
+	var drop1 = new DropItem(800,650,game,self);
+	var drop2 = new DropItem(1200,650,game,self);
 
 	dropZones.push(drop1);
 	dropZones.push(drop2);
-	
+
 	var selectedItem;
 
 	self.setSelectedItem=function(item){
 		selectedItem = item;
 	};
-	
+
 	self.addEventListener('touchmove',function(e){
 		//Ti.API.info("Touch move "+JSON.stringify(e));
-		if (selectedItem) { 
-			selectedItem.moveCenter(e.x,e.y); 		
+		if (selectedItem) {
+			selectedItem.moveCenter(e.x,e.y);
 		}
 	});
-	
+
 	self.addEventListener('touchend',function(e){
 		if (selectedItem){
 			Ti.API.info("touch end of item1"+JSON.stringify(e));
 			var foundZone = false;
 			for (var i = 0; i < dropZones.length; i++){
 				if (dropZones[i].contains(e.x,e.y)){
-					alert("Dropped!");
+					//alert("Dropped!"); 
+					dropZones[i].fillGlass();
+					selectedItem.returnHome();
 					foundZone = true;
 				}
 			}
